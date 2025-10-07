dcf1dad71850a99b6caf1ad3b016207df3db3481
Addison <animalcrossing8273@gmail.com>
2025-10-07 13:57:39 -0700

    made imu function more accurate and made a function to go in a straight line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import java.lang.Math;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Autonomous
 public class IMUTest extends LinearOpMode{
@@ -13,16 +13,45 @@ public class IMUTest extends LinearOpMode{
     @Override
     public void runOpMode(){
         robot = new RobotClass(hardwareMap);
+        ElapsedTime runTime = new ElapsedTime();
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         robot.imu.initialize(parameters);
         robot.imu.resetYaw();
         waitForStart();
+        runTime.reset();
         telemetry.addData("heading/angle", robot.getHeading());
-        turnByAngle(90);
+        driveStraight(3,1000);
+//        sleep(500);
+//        turnByAngle(90);
+//        sleep(5000);
+//        turnByAngle(-90);
     }
-    public void driveAuto(double left,double right) {
+    public void driveStraight(double power, long time /* long variable type is really big so its for time-based purposes */) { //left: left side power, right: right side power, time: for how long
+        robot.imu.resetYaw();
+        ElapsedTime Runtime = new ElapsedTime();
+        Runtime.reset();
+        long endtime = (time *  1000000) + Runtime.nanoseconds();
+        //set all motor powers
+        while (endtime != Runtime.nanoseconds()){
+            while (robot.getHeading() > 0){
+                driveFunction(power - .1, power + .1);
+            } while (robot.getHeading() < 0){
+                driveFunction( power+ .1, power - .1);
+            } while(robot.getHeading() == 0){
+                driveFunction(power, power);
+            }
+        }
+        //stop
+        driveStop();
+    } //STILL NEED TO TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public void driveBasic (double left, double right, long time){
+        driveFunction(left,right);
+        sleep(time);
+        driveStop();
+    }
+    public void driveFunction(double left,double right) {
         robot.frontLeft.setPower(left);
         robot.backLeft.setPower(left);
         robot.frontRight.setPower(right);
@@ -39,12 +68,20 @@ public class IMUTest extends LinearOpMode{
         double angleDistance;
         angleDistance = Math.abs(angle);
         if(angle < 0){
-            driveAuto(-.5,.5);
+            driveFunction(-.375,.375);
+            while(Math.abs(robot.getHeading()) < (angleDistance - 30)){
+                //empty like my soul
+            }
+            driveFunction(-.25,.25);
             while(Math.abs(robot.getHeading()) < angleDistance){
                 //EMPTY ON PURPOSE LOSER
             }
         } else {
-            driveAuto(.5,-.5);
+            driveFunction(.375,-.375);
+            while(Math.abs(robot.getHeading()) < (angleDistance - 30)){
+                //do I have to say it again?
+            }
+            driveFunction(.25, -.25);
             while(Math.abs(robot.getHeading()) < angleDistance){
                 //EMPTY ON PURPOSE EVEN WORSE LOSER
             }
