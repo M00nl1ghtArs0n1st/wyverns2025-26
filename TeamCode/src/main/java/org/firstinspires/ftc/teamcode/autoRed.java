2c3953f35d23eb6b6e19d3ffde0d960a0aba83e8
Addison <animalcrossing8273@gmail.com>
2025-10-27 17:02:01 -0700

    continued programming the auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRed.java
@@ -8,106 +8,108 @@ import com.qualcomm.robotcore.hardware.IMU;
 import java.lang.Math;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
 @Autonomous
-public class autored extends LinearOpMode{
+public class autoRed extends LinearOpMode {
     RobotClass robot;
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         robot = new RobotClass(hardwareMap);
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         robot.imu.initialize(parameters);
         robot.imu.resetYaw();
-
         waitForStart();
-        moveWithEncoders(100,100);//go to apriltag
+        moveWithEncoders(100,100);
         turnByAngle(-45);
         //scan april tag
         turnByAngle(45);
-        if(/* gpp */){
-            moveWithEncoders(-100,-25);
-            turnByAngle(90);
-            //activate intake
-            moveWithEncoders(50, 25);
-            //deactivate intake
-            moveWithEncoders(-50, -25);
-        }if(/* pgp */){
-            moveWithEncoders(-100, -50);
-            turnByAngle(90);
-            //activate intake
-            moveWithEncoders(50, 25);
-            //deactivate intake
-            moveWithEncoders(-50, -25);
-        } else /*ppg*/{
-            moveWithEncoders(-100,-75);
-            turnByAngle(90);
-            //activate intake
-            moveWithEncoders(50, 25);
-            //deactivate intake
-            moveWithEncoders(-50, -25);
-        }
+//        if(/* gpp */){
+
+
+//        }if(/* pgp */){
+//            moveWithEncoders(-100, -50);
+//            turnByAngle(90);
+//            //activate intake
+//            moveWithEncoders(50, 25);
+//            //deactivate intake
+//            moveWithEncoders(-50, -25);
+//        } else /*ppg*/{
+//            moveWithEncoders(-100,-75);
+//            turnByAngle(90);
+//            //activate intake
+//            moveWithEncoders(50, 25);
+//            //deactivate intake
+//            moveWithEncoders(-50, -25);
     }
+
     public void driveStraight(double power, long time /* long variable type is really big so its for time-based purposes */) {
         robot.imu.resetYaw();
         ElapsedTime Runtime = new ElapsedTime(); //makes a new timer
         Runtime.reset(); //resets this timer (if this is the second time the command has been used
-        long endtime = (time *  1000000) + Runtime.nanoseconds(); //calculates what time the robot needs to start at and converts it to nanoseconds so the variable remains a long
+        long endtime = (time * 1000000) + Runtime.nanoseconds(); //calculates what time the robot needs to start at and converts it to nanoseconds so the variable remains a long
         //set all motor powers
-        while (endtime >= Runtime.nanoseconds()){ // does things during the specified time
-            while (robot.getHeading() > 0){
+        while (endtime >= Runtime.nanoseconds()) { // does things during the specified time
+            while (robot.getHeading() > 0) {
                 driveFunction(power - .1, power + .1); //veers left
-            } while (robot.getHeading() < 0){
-                driveFunction( power+ .1, power - .1);//veers right
-            } while(robot.getHeading() == 0){
+            }
+            while (robot.getHeading() < 0) {
+                driveFunction(power + .1, power - .1);//veers right
+            }
+            while (robot.getHeading() == 0) {
                 driveFunction(power, power); // just goes straight
             }
         }
         driveStop(); //stops after the specified amount of time has passed
     } //STILL NEED TO TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    public void driveBasic (double left, double right, long time){
+
+    public void driveBasic(double left, double right, long time) {
         driveFunction(left, right);
         sleep(time);
         driveStop();
     }
-    public void driveFunction(double left,double right) {
+
+    public void driveFunction(double left, double right) {
         robot.frontLeft.setPower(left);
         robot.backLeft.setPower(left);
         robot.frontRight.setPower(right);
         robot.backRight.setPower(right);
     } //just sets motor powers
+
     public void driveStop() {
         robot.frontLeft.setPower(0);
         robot.backLeft.setPower(0);
         robot.frontRight.setPower(0);
         robot.backRight.setPower(0);
     } //stops all motors
-    public void turnByAngle (double angle){
+
+    public void turnByAngle(double angle) {
         robot.imu.resetYaw();
         double angleDistance;
         angleDistance = Math.abs(angle); //finds how far the robot needs to go
-        if(angle < 0){
-            driveFunction(-.375,.375); //sets initial motor powers
-            while(Math.abs(robot.getHeading()) < (angleDistance - 30)){
+        if (angle < 0) {
+            driveFunction(-.375, .375); //sets initial motor powers
+            while (Math.abs(robot.getHeading()) < (angleDistance - 30)) {
                 //empty like my soul
             }//waits until the robot only needs to turn 30 more degrees
-            driveFunction(-.25,.25); //robot slows down to be more accurate
-            while(Math.abs(robot.getHeading()) < angleDistance){
+            driveFunction(-.25, .25); //robot slows down to be more accurate
+            while (Math.abs(robot.getHeading()) < angleDistance) {
                 //EMPTY ON PURPOSE LOSER
             }// waits for the robot to turn all the way
         } else {
-            driveFunction(.375,-.375);//sets initial motor powers
-            while(Math.abs(robot.getHeading()) < (angleDistance - 30)){
+            driveFunction(.375, -.375);//sets initial motor powers
+            while (Math.abs(robot.getHeading()) < (angleDistance - 30)) {
                 //do I have to say it again?
             } //waits until the robot only has to turn 30 more degrees
             driveFunction(.25, -.25); //robot slows to be more accurate
-            while(Math.abs(robot.getHeading()) < angleDistance){
+            while (Math.abs(robot.getHeading()) < angleDistance) {
                 //EMPTY ON PURPOSE EVEN WORSE LOSER
             } //waits for the robot to turn to the specified angle
         }
         driveStop(); //stops robot after it has turned
     }
+
     public void moveWithEncoders(double velocity, int targetPosition) {
         robot.frontLeft.setTargetPosition(targetPosition);
         robot.frontRight.setTargetPosition(targetPosition);
@@ -117,7 +119,7 @@ public class autored extends LinearOpMode{
         robot.frontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         robot.backLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         robot.backRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        driveFunction(.5,.5);
+        driveFunction(.5, .5);
         robot.frontLeft.setVelocity(velocity);
         robot.frontRight.setVelocity(velocity);
         robot.backLeft.setVelocity(velocity);
